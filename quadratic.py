53338e2b44d6247558d3005904e16f99fe64d6f8
zukinkalitshana <zukinkalitshana@icloud.com>
2024-08-07 00:37:42 +0200

    Write example for finding pairs that equal a given target

--- quadratic.py
+++ quadratic.py
@@ -1,3 +1,5 @@
+from typing import List
+
 names = ["Zuki", "Andy", "Azz", "Thale", "Beast", "Lele", "Martian", "Diko"]
 
 class BigO:
@@ -29,6 +31,7 @@ class BigO:
        
        print(f"Sorted list: {names_list}")
 
+
     # Given 2 arrays, create a function that lets a user know whether the arrays contain any common elements
     # We return a Bool
     # The arrays can be sorted or unsorted. It doesn't matter. The time complexity is O(n^2).
@@ -42,11 +45,30 @@ class BigO:
     # Need to optimize this code. Can possibly be improved to O(n), but that's just time. What about space?
 
 
+    # Exercise: find a pair of consecutive numbers in a given circular array 'arr' that equals a specified target
+    # Assumptions: The array can grow in size. We return the numbers/ their indices if they exists, else an empty array
+    # (cont.) We're looping. Since the array grows, we might not always know its size. Therefore, the other loop is a while.
+    def number_pair_equal_to_target(self, arr1, target) -> List[int]:
+        pair = []
+        for i in range(len(arr1)-1):
+            if arr1[i] + arr1[i + 1] == target:
+                pair.append(i)
+                pair.append(i + 1)    
+        return pair
+
+
+
 big_o = BigO(names)
 # big_o.print_pairs()
 # big_o.bubble_sort()
 
-array1 = ['z', 'e', 'b', 'r', 'a', 'i', 'a', 'n']
-array2 = ['l', 'i', 'o', 'n']
-result = big_o.check_for_common_elements(array1, array2)
-print(f"The result is {result}")
+# array1 = ['z', 'e', 'b', 'r', 'a', 'i', 'a', 'n']
+# array2 = ['l', 'i', 'o', 'n']
+# result = big_o.check_for_common_elements(array1, array2)
+# print(f"The result is {result}")
+
+# Data set:
+numbers = [1, 3, 4, 2, 5, 3]
+target = 8 # Expected Result: [4,2]
+
+print(f"Elements at indices {big_o.number_pair_equal_to_target(numbers, target)}")
